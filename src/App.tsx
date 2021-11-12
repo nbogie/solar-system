905ac99534a4eeb4d032c0430c72430aefe334db
nbogie <neillbogie@googlemail.com>
2021-11-12 00:13:23 +0000

    Add CSS to put canvas behind text

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./App.css";
 import AppHeader from "./components/AppHeader";
 import NavBar from "./components/NavBar";
 import SolarSystem from "./components/SolarSystem";
-
+import './App.css';
 function App(): JSX.Element {
 
   //a place we can store state WITHOUT causing a re-render (and a re-render of the child component)
